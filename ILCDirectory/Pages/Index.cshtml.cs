927a38bc66a87f75f40b72c13a6438474d25da9c
Robb Sadler <robb@robbsadler.com>
2023-09-13 06:51:16 -0500

    ongoing

--- ILCDirectory/Pages/Index.cshtml.cs
+++ ILCDirectory/Pages/Index.cshtml.cs
@@ -8,15 +8,21 @@ namespace ILCDirectory.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IILCDirectoryRepository _repo;
+        private readonly IConfiguration _cfg;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration cfg, IILCDirectoryRepository repo)
         {
             _logger = logger;
+            _repo = repo;
+            _cfg = cfg;
         }
 
-        public void OnGet()
-        {
+        public IList<Person> Persons { get; set; }
 
+        public async Task OnGet()
+        {
+            Persons = await _repo.GetAllPersonsAsync(_cfg);
         }
     }
 }
